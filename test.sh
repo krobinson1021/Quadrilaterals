a93fa139cd341b364f62773b9fd626a7788302bf
Katie <katie.robinson@utah.edu>
2019-01-30 19:24:43 -0700

    troubleshooting squares

--- test.sh
+++ test.sh
@@ -1,8 +1,2 @@
 ./main < squares.txt > output.txt
-./main < rectangles.txt >> output.txt
-./main < parallelograms.txt >> output.txt
-./main < rhombi.txt >> output.txt
-./main < kites.txt >> output.txt
-./main < trapezoids.txt >> output.txt
-./main < quadrilaterals.txt >> output.txt
-diff output.txt expected_output.txt
+diff output.txt f.txt
