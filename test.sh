6aa3ec459c1ddc3dc1dafa85289d3239b504abb2
Katie <katie.robinson@utah.edu>
2019-01-30 20:09:28 -0700

    fixed line bug

--- test.sh
+++ test.sh
@@ -1,2 +1,8 @@
 ./main < squares.txt > output.txt
-diff output.txt f.txt
+./main < rectangles.txt >> output.txt
+./main < parallelograms.txt >> output.txt
+./main < rhombi.txt >> output.txt
+./main < kites.txt >> output.txt
+./main < trapezoids.txt >> output.txt
+./main < quadrilaterals.txt >> output.txt
+diff output.txt expected_output.txt
