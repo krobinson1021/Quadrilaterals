1685419ec39ca75649209edce5b2cf49ef5ffe61
Katie <katie.robinson@utah.edu>
2019-01-30 19:05:59 -0700

    corrected test.sh file error

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 ./main < rectangles.txt >> output.txt
 ./main < parallelograms.txt >> output.txt
 ./main < rhombi.txt >> output.txt
-./main < trapezoids.txt >> output.txt
 ./main < kites.txt >> output.txt
+./main < trapezoids.txt >> output.txt
 ./main < quadrilaterals.txt >> output.txt
 diff output.txt expected_output.txt
