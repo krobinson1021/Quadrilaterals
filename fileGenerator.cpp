40817e31056045365334a05d3f76b64470aedb62
Katie <katie.robinson@utah.edu>
2019-02-04 15:57:26 -0700

    moved old Travis files into archive folder, randomly generated error test files with generator.cpp

--- fileGenerator.cpp
+++ fileGenerator.cpp
@@ -11,13 +11,13 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     
+    // generating 1000 random text files with valid quadrilateral data
     vector<vector<string> > allLines;
     string line;
     string delimiter = " ";
     ifstream infile;
-    
-
     infile.open ("allQuads_input.txt");
+
     while(!infile.eof()) {
         getline(infile,line);
         istringstream ss(line);
@@ -30,7 +30,7 @@ int main(int argc, const char * argv[]) {
     infile.close();
 
     for (int i = 1; i <= 1000; i++) {
-        string filename = "/Users/katierobinson/Quadrilaterals/validQuadTesting/" + to_string(i) + ".txt";
+        string filename = "validQuadTesting/" + to_string(i) + ".txt";
         ofstream textFile (filename);
         srand (time(NULL));
         int scalingFactor = rand() % 10 + 1; // nothing in the input text file has a coordinate bigger than 10
@@ -48,5 +48,88 @@ int main(int argc, const char * argv[]) {
         textFile.close();
     }
 
-    return 0;
+    // generating 10 random text files with error1 data
+    for (int i = 1; i <= 10; i++) {
+        string filename = "invalidError1Testing/" + to_string(i) + ".txt";
+        ofstream textFile (filename);
+        int numToPrint = rand() % 20 + 1;
+        for (int k = 0; k < numToPrint; k++) {    
+            int r = rand() % 78;   // generate a random number
+            char c = '0' + r;      // Convert to a char
+            textFile << c;
+            if (r % 2 == 0) {
+                textFile << " ";
+            }
+            if (k == 4 || k == 7 || k == 9 || k == 11) {
+                textFile << "\n";
+            }
+            if (k == 15) {
+                textFile << "\n\n";
+            }
+        }
+    }
+
+    // generating 10 random text files with error2 data
+    for (int i = 1; i <= 10; i++) {
+        string filename = "invalidError2Testing/" + to_string(i) + ".txt";
+        ofstream textFile (filename);
+        for (int k = 0; k < 4; k++) {    
+            int r = rand() % 101;   // generate a random number between 1 and 100
+            textFile << r;
+            if (k < 5) {
+                textFile << " ";
+            }
+        }
+        textFile << 0 << " " << 0; // guarantees a point will coincide
+    }
+
+    // generating 10 random text files with error3 data
+    int crossingCoordinates1[6] = {3, 3, 4, 7, 8, 9};
+    int crossingCoordinates2[6] = {1, 0, 1, 2, 2, 1};
+    for (int i = 1; i <= 10; i++) {
+        string filename = "invalidError3Testing/" + to_string(i) + ".txt";
+        ofstream textFile (filename);
+        int scalingFactor = rand() % 10 + 1;   // generate a random number between 1 and 10
+        if (i < 5) {
+            for (int k = 0; k < 6; k++) {
+                textFile << (scalingFactor * crossingCoordinates1[k]);
+                if (k != 5) {
+                    textFile << " ";
+                }
+            }
+        } else {
+            for (int k = 0; k < 6; k++) {
+                textFile << (scalingFactor * crossingCoordinates2[k]);
+                if (k != 5) {
+                    textFile << " ";
+                }
+            }
+        }
+    }
+
+    // generating 10 random text files with error4 data
+    int colinearY[6] = {9, 1, 2, 1, 2, 1};
+    int colinearX[6] = {1, 4, 1, 3, 1, 8};
+    for (int i = 1; i <= 10; i++) {
+        string filename = "invalidError4Testing/" + to_string(i) + ".txt";
+        ofstream textFile (filename);
+        int scalingFactor = rand() % 10 + 1;   // generate a random number between 1 and 10
+        if (i < 5) {
+            for (int k = 0; k < 6; k++) {
+                textFile << (scalingFactor * colinearY[k]);
+                if (k != 5) {
+                        textFile << " ";
+                }
+            }
+        } else {
+           for (int k = 0; k < 6; k++) {
+                textFile << (scalingFactor * colinearX[k]);
+                if (k != 5) {
+                        textFile << " ";
+                }
+            } 
+        }
+    }
+
 }
+
